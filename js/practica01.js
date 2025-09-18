0a34dfcbaa68735bb7c20ffddc7a8e106d79dbc0
MiltonJJP <miltonjaimes11@gmail.com>
2025-09-17 21:27:18 -0600

    fix: download report and responsive navbar

--- js/practica01.js
+++ js/practica01.js
@@ -1,26 +1,7 @@
 //variables
-const documento = document.querySelector('.technical-sheet');
-const buttonSheet = document.getElementsByClassName('card-link');
 
-const cardDiv = document.querySelector('.card-div');
 
 
 //eventos
 
 
-cardDiv.addEventListener('click', (e)=>{
-    if(e.target.classList.contains('card-link')){
-        console.log('click en la ficha técnica');
-    }
-})
-
-window.addEventListener('scroll', () =>{
-    const nav = document.querySelector('.navbar');
-    const ubicacion = nav.getBoundingClientRect();
-
-    if(ubicacion.top > -200){
-        
-    } else{
-        console.log('no es visible');
-    }
-});
